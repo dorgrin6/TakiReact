50775be63619ae61fb3551002f84f29efb4e1f23
Idan Goor <idangoo@mta.ac.il>
2018-05-30 18:11:44 +0300

    Added closeTakiButton and updated superTaki textures

--- src/components/PlayZone.js
+++ src/components/PlayZone.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "./Card.js";
+import CloseTakiButton from "./CloseTakiButton.js";
+import manager from "../engine/Manager.js";
 
 export default class PlayZone extends React.Component {
   constructor(props) {
@@ -18,6 +20,15 @@ export default class PlayZone extends React.Component {
     };
 
     this.cardsWithAngle.push(playZoneCard);
+  }
+
+  checkTakiMode() {
+    let activePlayer = manager.getActivePlayer();
+    return (activePlayer.inTakiMode.status === true && activePlayer.playerType === "user");
+  }
+
+  closeTaki(){
+      manager.getActivePlayer().closeTaki();
   }
 
   renderPlayZoneCard(card){
@@ -39,11 +50,13 @@ export default class PlayZone extends React.Component {
     }
 
     for(var i=0;i<this.cardsWithAngle.length;i++){
-        this.cardsWithAngle[i].card=this.props.playZone.cards[i];
+      this.cardsWithAngle[i].card=this.props.playZone.cards[i];
     }
 
+    let isInTakiMode = this.checkTakiMode();
     return (
       <div className={"playZone"} id={"playZone"}>
+        {isInTakiMode ? <CloseTakiButton onClick={()=>this.closeTaki()}/> : null}
         {this.cardsWithAngle.map(card => this.renderPlayZoneCard(card))}
       </div>
     );
